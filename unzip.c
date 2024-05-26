fd3cbc3a5eb22534d1ff9692a038dc03994f12c8
Ivan Sorokin <unxed@mail.ru>
2024-05-26 15:57:21 +0200

    Fix several problems in codepages support: 1) Fixed bit 11 of General purpose flag support on systems with UTF-8 system charset 2) Fixed OEM code page being always assumed Russian/Cyrillic CP866 on any UTF-8 system 3) Added proper OEM code page detection based on system locale setting 4) Removed translation from ISO 8859-1 to local charset; assumption that any non-unicode archive uses it is for sure wrong as it can be any charset used on archive creator's local system; also do not treat PKZIP for UNIX 2.51 archives as having ISO 8859-1 charset for the same reasons 5) Enabled UTF-8 output by default on Unix systems 6) Fix some legacy ANSI archives support 7) -I and -O for manually setting charset, they are just the same now, for all types of archives with 1-byte charset, both left for compatibility

--- unzip.c
+++ unzip.c
@@ -1362,7 +1362,7 @@ int uz_opts(__G__ pargc, pargv)
 
 #ifdef UNIX
     extern char OEM_CP[MAX_CP_NAME];
-    extern char ISO_CP[MAX_CP_NAME];
+    extern int forcedCP;
 #endif
 
     while (++argv, (--argc > 0 && *argv != NULL && **argv == '-')) {
@@ -1547,33 +1547,35 @@ int uz_opts(__G__ pargc, pargv)
                     break;
 #endif  /* MACOS */
 #ifdef UNIX
-    			case ('I'):
+                case ('I'):
                     if (negative) {
                         Info(slide, 0x401, ((char *)slide,
                           "error:  encodings can't be negated"));
                         return(PK_PARAM);
-    				} else {
-    					if(*s) { /* Handle the -Icharset case */
-    						/* Assume that charsets can't start with a dash to spot arguments misuse */
-    						if(*s == '-') {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -I argument"));
-    	                        return(PK_PARAM);
-    						}
-    						strncpy(ISO_CP, s, sizeof(ISO_CP));
-    					} else { /* -I charset */
-    						++argv;
-    						if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -I argument"));
-    	                        return(PK_PARAM);
-    						}
-    						s = *argv;
-    						strncpy(ISO_CP, s, sizeof(ISO_CP));
-    					}
-    					while(*(++s)); /* No params straight after charset name */
-    				}
-    				break;
+                    } else {
+                        if(*s) { /* Handle the -Icharset case */
+                            /* Assume that charsets can't start with a dash to spot arguments misuse */
+                            if(*s == '-') {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -I argument"));
+                                return(PK_PARAM);
+                            }
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        } else { /* -I charset */
+                            ++argv;
+                            if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -I argument"));
+                                return(PK_PARAM);
+                            }
+                            s = *argv;
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        }
+                        while(*(++s)); /* No params straight after charset name */
+                    }
+                    break;
 #endif /* ?UNIX */
                 case ('j'):    /* junk pathnames/directory structure */
                     if (negative)
@@ -1651,33 +1653,35 @@ int uz_opts(__G__ pargc, pargv)
                         ++uO.overwrite_all;
                     break;
 #ifdef UNIX
-    			case ('O'):
+                case ('O'):
                     if (negative) {
                         Info(slide, 0x401, ((char *)slide,
                           "error:  encodings can't be negated"));
                         return(PK_PARAM);
-    				} else {
-    					if(*s) { /* Handle the -Ocharset case */
-    						/* Assume that charsets can't start with a dash to spot arguments misuse */
-    						if(*s == '-') {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -I argument"));
-    	                        return(PK_PARAM);
-    						}
-    						strncpy(OEM_CP, s, sizeof(OEM_CP));
-    					} else { /* -O charset */
-    						++argv;
-    						if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -O argument"));
-    	                        return(PK_PARAM);
-    						}
-    						s = *argv;
-    						strncpy(OEM_CP, s, sizeof(OEM_CP));
-    					}
-    					while(*(++s)); /* No params straight after charset name */
-    				}
-    				break;
+                    } else {
+                        if(*s) { /* Handle the -Ocharset case */
+                            /* Assume that charsets can't start with a dash to spot arguments misuse */
+                            if(*s == '-') {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -I argument"));
+                                return(PK_PARAM);
+                            }
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        } else { /* -O charset */
+                            ++argv;
+                            if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -O argument"));
+                                return(PK_PARAM);
+                            }
+                            s = *argv;
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        }
+                        while(*(++s)); /* No params straight after charset name */
+                    }
+                    break;
 #endif /* ?UNIX */
                 case ('p'):    /* pipes:  extract to stdout, no messages */
                     if (negative) {
--- zipinfo.c
+++ zipinfo.c
@@ -459,7 +459,7 @@ int zi_opts(__G__ pargc, pargv)
 
 #ifdef UNIX
     extern char OEM_CP[MAX_CP_NAME];
-    extern char ISO_CP[MAX_CP_NAME];
+    extern int forcedCP;
 #endif
 
 #ifdef MACOS
@@ -506,33 +506,35 @@ int zi_opts(__G__ pargc, pargv)
                     }
                     break;
 #ifdef UNIX
-    			case ('I'):
+                case ('I'):
                     if (negative) {
                         Info(slide, 0x401, ((char *)slide,
                           "error:  encodings can't be negated"));
                         return(PK_PARAM);
-    				} else {
-    					if(*s) { /* Handle the -Icharset case */
-    						/* Assume that charsets can't start with a dash to spot arguments misuse */
-    						if(*s == '-') {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -I argument"));
-    	                        return(PK_PARAM);
-    						}
-    						strncpy(ISO_CP, s, sizeof(ISO_CP));
-    					} else { /* -I charset */
-    						++argv;
-    						if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -I argument"));
-    	                        return(PK_PARAM);
-    						}
-    						s = *argv;
-    						strncpy(ISO_CP, s, sizeof(ISO_CP));
-    					}
-    					while(*(++s)); /* No params straight after charset name */
-    				}
-    				break;
+                    } else {
+                        if(*s) { /* Handle the -Icharset case */
+                            /* Assume that charsets can't start with a dash to spot arguments misuse */
+                            if(*s == '-') {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -I argument"));
+                                return(PK_PARAM);
+                            }
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        } else { /* -I charset */
+                            ++argv;
+                            if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -I argument"));
+                                return(PK_PARAM);
+                            }
+                            s = *argv;
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        }
+                        while(*(++s)); /* No params straight after charset name */
+                    }
+                    break;
 #endif /* ?UNIX */
                 case 'l':      /* longer form of "ls -l" type listing */
                     if (negative)
@@ -555,33 +557,35 @@ int zi_opts(__G__ pargc, pargv)
                     break;
 #endif
 #ifdef UNIX
-    			case ('O'):
+                case ('O'):
                     if (negative) {
                         Info(slide, 0x401, ((char *)slide,
                           "error:  encodings can't be negated"));
                         return(PK_PARAM);
-    				} else {
-    					if(*s) { /* Handle the -Ocharset case */
-    						/* Assume that charsets can't start with a dash to spot arguments misuse */
-    						if(*s == '-') {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -I argument"));
-    	                        return(PK_PARAM);
-    						}
-    						strncpy(OEM_CP, s, sizeof(OEM_CP));
-    					} else { /* -O charset */
-    						++argv;
-    						if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
-    	                        Info(slide, 0x401, ((char *)slide,
-        		                  "error:  a valid character encoding should follow the -O argument"));
-    	                        return(PK_PARAM);
-    						}
-    						s = *argv;
-    						strncpy(OEM_CP, s, sizeof(OEM_CP));
-    					}
-    					while(*(++s)); /* No params straight after charset name */
-    				}
-    				break;
+                    } else {
+                        if(*s) { /* Handle the -Ocharset case */
+                            /* Assume that charsets can't start with a dash to spot arguments misuse */
+                            if(*s == '-') {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -I argument"));
+                                return(PK_PARAM);
+                            }
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        } else { /* -O charset */
+                            ++argv;
+                            if(!(--argc > 0 && *argv != NULL && **argv != '-')) {
+                                Info(slide, 0x401, ((char *)slide,
+                                  "error:  a valid character encoding should follow the -O argument"));
+                                return(PK_PARAM);
+                            }
+                            s = *argv;
+                            strncpy(OEM_CP, s, sizeof(OEM_CP));
+                            forcedCP = 1;
+                        }
+                        while(*(++s)); /* No params straight after charset name */
+                    }
+                    break;
 #endif /* ?UNIX */
                 case 's':      /* default:  shorter "ls -l" type listing */
                     if (negative)
